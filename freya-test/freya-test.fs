4c1a5f6f5edabab4cd7e95001bcad883b0fbd74c
Eadred <eadred.birchenough@zuhlke.com>
2016-12-16 09:42:46 +0000

    Added implementation and updated target framework

--- freya-test/freya-test.fs
+++ freya-test/freya-test.fs
@@ -1,6 +1,44 @@
 module FreyaTest
 
+open Freya.Core
+open Freya.Machines.Http
+open Freya.Routers.Uri.Template
+open System
+open Microsoft.Owin.Hosting
+
+let name =
+  freya {
+    let! name = Freya.Optic.get (Route.atom_ "name")
+
+    match name with
+    | Some name -> return name
+    | _ -> return "World"
+  }
+
+let hello =
+  freya {
+    let! name = name
+
+    return Represent.text (sprintf "Hello %s!" name)
+  }
+
+let machine =
+  freyaMachine {
+    handleOk hello
+  }
+
+let router =
+  freyaRouter {
+    resource "/hello{/name}" machine
+  }
+
+type HelloWorld () =
+  member __.Configure () =
+    OwinAppFunc.ofFreya router
+
 [<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+let main _ =
+  Console.WriteLine "Starting"
+  WebApp.Start<HelloWorld> "http://localhost;7000" |> ignore
+  Console.ReadLine () |> ignore
+  0
