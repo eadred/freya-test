d7c372e3e40655088cdaaf7bcd5a09b8fa636424
Eadred <eadred.birchenough@zuhlke.com>
2016-12-19 09:37:21 +0000

    Implemented some simple authorization

--- freya-test/freya-test.fs
+++ freya-test/freya-test.fs
@@ -15,16 +15,31 @@ let name =
     | _ -> return "World"
   }
 
+let responseHeader =
+  freya {
+    return! Freya.Optic.set (Freya.Optics.Http.Response.header_ "MyHeader") (Some "Hello")
+  }
+
 let hello =
   freya {
-    do! Freya.Optic.set (Freya.Optics.Http.Response.header_ "MyHeader") (Some "Hello")
+    do! responseHeader
     let! name = name
 
     return Represent.text (sprintf "Hello %s!" name)
   }
 
+let checkAuthorized =
+  let isAuthorized h = "alakazam".Equals(h)
+  freya {
+    let! authHeader = Freya.Optic.get (Freya.Optics.Http.Request.header_ "Authorization")
+    match authHeader with
+    | Some h -> return isAuthorized h
+    | None -> return false
+  }
+
 let machine =
   freyaMachine {
+    authorized checkAuthorized
     handleOk hello
   }
 
